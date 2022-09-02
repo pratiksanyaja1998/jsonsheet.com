0917809bad2d224377ef293b4191310b77b06910
Pratik Sanyaja <pratiksanyaja1998@gmail.com>
2022-09-02 18:04:53 +0530

    WIP - fix bugs

--- src/pages/template/[templateName]/[...templateId].js
+++ src/pages/template/[templateName]/[...templateId].js
@@ -12,39 +12,11 @@ const Template = (props) => {
     const [isLoading, setIsLoading] = useState(false);
     const [details, setDetails] = useState(null);
 
-    let testdata = {
-        category: 4,
-        description: "With this template, you can stay on top of your cases, clients, and billing, no matter what's thrown at you.",
-        id: 4,
-        image: 'https://s3.ap-south-1.amazonaws.com/assets.whitelabelapp.in/image/googlesheetapp/9d5f0459-5657-4f00-946a-f40c7e74ceee.jpg',
-        name: 'Legal Case Tracking and billing',
-        slug: 'legal-case-tracking-and-billing',
-        url: 'https://docs.google.com/spreadsheets/d/1oeI22Qn4TIm0V9itIKT-hzuK_851K3W_zNBCATGbdCg'
-    };
-
     useEffect(() => {
-        fatchDetail();
+        fetchDetail();
     }, []);
 
-    const fatchData = () => {
-        setIsLoading(true);
-        let url = 'https://api.whitelabelapp.in/googlesheetapp/templates/list';
-        if (search) {
-            url += '?search=' + search;
-        }
-        axios
-            .get(url)
-            .then((res) => {
-                console.log('res list-->', res.data);
-                setTemplateList(res.data);
-                setIsLoading(false);
-            })
-            .catch((err) => {
-                console.log(err);
-                setIsLoading(false);
-            });
-    };
-    const fatchDetail = () => {
+    const fetchDetail = () => {
         setIsLoading(true);
         let url = `https://api.whitelabelapp.in/googlesheetapp/templates/details/${props.params.templateId}`;
         // if (search) {
@@ -94,11 +66,22 @@ const Template = (props) => {
     );
 };
 export const getStaticPaths = async () => {
-    return {
-        paths: [], //indicates that no page needs be created at build time
+    const list = await axios.get('https://api.whitelabelapp.in/googlesheetapp/templates/list').then((res) => res.data);
 
-        fallback: 'blocking' //indicates the type of fallback
-    };
+    let paths =
+        list?.length > 0 &&
+        list?.map((o) => ({
+            params: {
+                templateName: o.slug,
+                templateId: [o.id.toString()]
+            }
+        }));
+    return { paths, fallback: false };
+    // return {
+    //     paths: [], //indicates that no page needs be created at build time
+
+    //     fallback: 'blocking' //indicates the type of fallback
+    // };
 };
 export async function getStaticProps({ params }) {
     const props = await sourcebitDataClient.getStaticPropsForPageAtPath('');
