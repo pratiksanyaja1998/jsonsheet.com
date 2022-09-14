aeb96e1d31a554ad0f04466ec1101c7e9f303d2c
Pratik Sanyaja <pratiksanyaja1998@gmail.com>
2022-09-14 15:49:31 +0530

    WIP - add canonical link

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,25 @@
 import '../sass/main.scss';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useRouter } from 'next/router';
+import { Helmet } from 'react-helmet';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+    const defaultUrl = 'https://jsonsheet.com/';
+
+    const router = useRouter();
+    const _pathSliceLength = Math.min.apply(Math, [
+        router.asPath?.indexOf('?') > 0 ? router.asPath.indexOf('?') : router.asPath?.length,
+        router.asPath?.indexOf('#') > 0 ? router.asPath.indexOf('#') : router.asPath?.length
+    ]);
+    const canonicalURL = defaultUrl + router.asPath.substring(1, _pathSliceLength);
+
     return (
         <>
+            <Helmet>
+                <link rel="canonical" href={canonicalURL} />
+            </Helmet>
             <Component {...pageProps} />
             <ToastContainer />
         </>
